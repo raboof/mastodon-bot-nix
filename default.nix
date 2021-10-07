8dbb63ee3c3887c98f0c8ae6502524a9628cef75
Arnout Engelen <arnout@bzzt.net>
2021-10-07 11:02:11 +0200

    mastodon-bot-slim

--- default.nix
+++ default.nix
@@ -6,6 +6,10 @@
 
 let
   pkgs = import nixpkgs {};
+  mastodon-bot-slim = pkgs.runCommand "mastodon-bot-slim" {} ''
+    cp -r ${pkgs.mastodon-bot} $out
+    #find $out -type f -print0 | while IFS= read -r -d $'\0' f; do substituteInPlace $f --replace "${pkgs.nodejs}" "${pkgs.nodejs-slim}"; done
+  '';
   initScript = pkgs.writeScript "init" ''
     #!${pkgs.stdenv.shell}
 
@@ -13,9 +17,9 @@ let
 
     export MASTODON_BOT_CONFIG=${config-edn}
     export MASTODON_BOT_CREDENTIALS=${credentials-edn}
-    exec ${pkgs.mastodon-bot}/bin/mastodon-bot
+    exec ${mastodon-bot-slim}/bin/mastodon-bot
   '';
-  lib = pkgs.stdenv.lib;
+  lib = pkgs.lib;
 in lib.mkForce (pkgs.callPackage (nixpkgs + "/nixos/lib/make-system-tarball.nix") {
   fileName = "mastodon-bot-${builtins.baseNameOf config-edn}";
   storeContents = [
